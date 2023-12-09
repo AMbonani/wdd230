b11dbf930ccbe6e9228ea81567957ac262cfaa23
AMbonani <refilwe.given@gmail.com>
2023-12-09 07:14:01 +0200

    weather api

---
+++ week05/scripts/weather.js
@@ -0,0 +1,42 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+const latitude = 49.74990967757486;
+const longitude = 6.637441024950051;
+
+const apiKey = '77bf00e20f235f8f556baa0e89701021';
+
+const completeUrl = `${url}?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(completeUrl);
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+  const iconCode = data.weather[0].icon;
+  const iconsrc = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', data.weather[0].description);
+
+  captionDesc.textContent = `${data.weather[0].description}`;
+}
