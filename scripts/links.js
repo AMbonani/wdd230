88ed9f7cdbe5d7a13c86b6751c06682dbd96cdcf
AMbonani <refilwe.given@gmail.com>
2023-12-12 14:29:23 +0200

    links update

--- scripts/links.js
+++ scripts/links.js
@@ -1,7 +1,5 @@
-
 const baseURL = "https://AMbonani.github.io/wdd230/";
 const linksURL = baseURL + "data/links.json";
-
 
 async function getLinks() {
   try {
@@ -15,39 +13,37 @@ async function getLinks() {
 
 getLinks();
 
-
 function displayLinks(weeks) {
-  
   const linksContainer = document.getElementById('learning-activities');
 
- 
   linksContainer.innerHTML = '';
 
- 
   weeks.forEach(week => {
-    
     const listItem = document.createElement('li');
 
-    
-    week.links.forEach(activity => {
+    week.links.forEach((activity, index) => {
       const link = document.createElement('a');
       link.href = baseURL + activity.url;
       link.textContent = activity.title;
 
-     
       listItem.appendChild(link);
 
-      
-      const separator = document.createTextNode(' | ');
-      listItem.appendChild(separator);
+      if (index < week.links.length - 1) {
+        const separator = document.createElement('span');
+        separator.textContent = ' | ';
+        listItem.appendChild(separator);
+      }
     });
 
-    
-    listItem.lastChild.remove();
+    // Remove the last separator
+    if (listItem.lastChild.nodeType === Node.ELEMENT_NODE) {
+      listItem.lastChild.remove();
+    }
 
+    // Set the text content to include week information
     listItem.textContent = `${week.week}: ` + listItem.textContent;
 
-   
+    // Append the modified list item to the container
     linksContainer.appendChild(listItem);
   });
 }
